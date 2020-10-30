f648f443e3f5285a43e6b558cf702d7ef51a8acc
erikn775 <erikn775@gmail.com>
2020-10-30 09:36:15 -0600

    commit tree f65d7dc6019c82c1b7fb8be0461021e6653a258f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+        
+    def match(word_list)
+        new_array = []
+        word_list.each do |maybe_ana|
+            if maybe_ana.split("").sort == word.split("").sort
+                new_array.push(maybe_ana)
+            end
+        end
+
+        return new_array
+    end
+end
+  
+
+
+
+
